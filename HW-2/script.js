83238523f714def31b08a0bcbcb25bdaa071ec99
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-01-21 23:13:15 -0700

    console.log content

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,1 +1,14 @@
-console.log ("boop");
+console.log("Top Three Websites");
+console.log("1 - DeviantArt");
+console.log("2 - YouTube");
+console.log("3 - Pinterest");
+
+console.log("Top Three Games");
+console.log("1 - Animal Crossing: New Horizons");
+console.log("2 - Stardew Valley");
+console.log("3 - Overwatch");
+
+console.log("Top Three Artists");
+console.log("1 - Vincent van Gogh");
+console.log("2 - Whoever does the art for Studio Ghibli (Hayao Miyazaki?)");
+console.log("3 - Bob Ross");
