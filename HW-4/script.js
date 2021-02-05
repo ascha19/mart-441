eb355d3274aee179b17dcac1cf63af90a0f4919a
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-02-04 20:31:04 -0700

    added more options + story content

--- HW-4/script.js
+++ HW-4/script.js
@@ -9,14 +9,44 @@ function getOption1() {
   console.log(myStory);
 
   if(myOption === "Pick up cellphone") {
-    myStory.InnerHTML = "You pick up the phone and examine it. "
+    document.getElementById("option").style.display="none";
+    document.getElementById("submit").style.display="none";
+
+    document.getElementById("option2").style.display="block";
+    document.getElementById("submit2").style.display="block";
+
+    myStory.InnerHTML = "You pick up the phone and examine it. The screen is smashed but it is otherwise in tact and there is a piece of paper with a number on it behind the clear phone case: 0396. Suddenly, it rings. You can: >Answer or >Leave it alone.";
   }
 
   else if(myOption === "Get up and leave") {
-    myStory.InnerHTML = "You stand and make your way into the street."
+    myStory.InnerHTML = "You stand and make your way into the street. You're now standing in the middle of the road. You can >Go left or >Go right.";
   }
 
   else {
-    myStory.InnerHTML = "Invalid answer. Type either 'Pick up cellphone' or 'Get up and leave'."
+    myStory.InnerHTML = "Invalid answer. Type either 'Pick up cellphone' or 'Get up and leave'.";
   }
+}
+
+function getOption2() {
+  var myOption2 = document.getElementById("option2").value;
+
+  if (myOption2 === "Answer") {
+
+    document.getElementById("option2").style.display="none";
+    document.getElementById("submit2").style.display="none";
+
+    document.getElementById("option3").style.display="block";
+    document.getElementById("submit3").style.display="block";
+
+    myStory.InnerHTML = "You answer the phone. Before you can say a word, a terrified scream erupts from the speaker that makes you shudder; then the line goes dead. You can >Investigate or >Ignore it.";
+  }
+
+  else if (myOption2 === "Leave it alone") {
+    myStory.InnerHTML = "Instead of answering, you silence the phone and place it back on the bench. You lay back down and prepare to sleep again. You can >Dream or >Not dream.";
+  }
+
+  else {
+    myStory.InnerHTML = "Invalid answer. Type either 'Answer' or 'Leave it alone'.";
+  }
+
 }
