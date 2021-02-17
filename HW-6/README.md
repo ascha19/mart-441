88e50ca2c3e6a47ab7191777d3522a84c52c6a06
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-02-17 01:29:32 -0700

    copied hw-5 to hw-6 - initial setup

---
+++ HW-6/README.md
@@ -0,0 +1,7 @@
+# Summary
+
+This week was the initial setup of a memory card game. The original idea I had was to use tarot cards but then I decided that would be too many cards to work with for the start of the project. I settled for an easier idea, the twelve zodiac constellations. I found an image that included all of them online and then I cropped the image twelve times to make each of the individual 1x1 inch cards; then I found a pink nebula image that I thought would look good with the dusky purple background and yellow font colors that I wanted to use, and cropped it down as well.
+
+# Issues
+
+I had several mini syntax issues that caused my code to break but other than that, this week's coding went pretty smoothly. The first of them was having an extra '{' where it wasn't supposed to be and another was not adding enough zeroes to my count variable. One other minor issue I had was that the border of my divs that held the cards were twice the width than I wanted them to be. This issue I still haven't fixed but it doesn't negatively affect the code at all, so I'm not worrying about it for the time being.
---
+++ HW-6/script.js
@@ -0,0 +1,39 @@
+console.log(document.title);
+console.log(document.URL);
+console.dir(document);
+
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12"];
+
+var imageBackPath = "./imgs/nebula-back.jpg";
+
+var matchingImages = new Array();
+
+function cardBacks() {
+
+  randomizedImage();
+    for(var i = 0; i <imageTags.length; i++) {
+      document.getElementById(imageTags[i]).src= imageBackPath;
+    }
+}
+
+function randomizedImage() {
+
+  var matchingImagesPath = ["./imgs/aries.jpg", "./imgs/taurus.jpg", "./imgs/gemini.jpg", "./imgs/cancer.jpg", "./imgs/leo.jpg", "./imgs/virgo.jpg", "./imgs/libra.jpg", "./imgs/scorpio.jpg", "./imgs/sag.jpg", "./imgs/capricorn.jpg", "./imgs/aquarius.jpg", "./imgs/pisces.jpg"];
+
+  var count = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+  while(matchingImages.length < 12) {
+
+    var randomNumber = Math.floor(Math.random() * matchingImagesPath.length)
+
+    if(count[randomNumber] < 2) {
+      matchingImages.push(matchingImagesPath[randomNumber]);
+
+      count[randomNumber] = count[randomNumber] + 1;
+    }
+  }
+}
+
+function flipCard(number) {
+  document.getElementById(imageTags[number]).src= matchingImages[number];
+}
