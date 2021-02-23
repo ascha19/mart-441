64fed5c2b0147e578efc1cf256fcf615dd5e4d2f
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-02-22 17:46:51 -0700

    reorganized files

--- HW-6/script.js
+++ HW-6/script.js
@@ -2,12 +2,25 @@ console.log(document.title);
 console.log(document.URL);
 console.dir(document);
 
+//Variable for image tags and back of cards
 var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12"];
-
 var imageBackPath = "./imgs/nebula-back.jpg";
 
+//Varialble for numbers
+var numberUno = -1;
+var numberDos = -1;
+
+//Variables for amount of moves made and final score
+var move = 0;
+var foundAllMatches = 0;
+
+//Variable for player information (JSON)
+var playerInfo = {"firstName": "", "lastName":"", "age":""};
+
+//Variable for card array
 var matchingImages = new Array();
 
+//Setting up randomized images and making cards appear
 function cardBacks() {
 
   randomizedImage();
@@ -34,6 +47,75 @@ function randomizedImage() {
   }
 }
 
+//Function to flip cards and add score
 function flipCard(number) {
   document.getElementById(imageTags[number]).src= matchingImages[number];
+
+  if(numberUno >= 0) {
+    numberDos = number;
+    document.getElementById(imageTags[number]).src = matchingImages[numberDos];
+  }
+  else if(numberUno < 0) {
+    numberUno = number;
+    document.getElementById(imageTags[numberUno]).src = matchingImages[numberUno];
+  }
+
+  if(matchingImages[numberDos] != matchingImages[numberUno] && numberUno >= 0 && numberDos >=0) {
+    move++;
+    setTimeout(goodbyeCards, 1000);
+  }
+  else if(matchingImages[numberDos] == matchingImages[numberUno] && numberUno >= 0 && numberDos >= 0) {
+    move++;
+    foundAllMatches++;
+
+    numberUno = -1;
+    numberDos = -1;
+
+    if(foundAllMatches == matchingImages.length/2) {
+      playerInfo.move = move;
+      localStorage.setItem("readyPlayerOne", JSON.stringify(playerInfo));
+      window.location = "finalscreen.html";
+    }
+  }
+
+  if(foundAllMatches >= 5) {
+    window.location = "index.html";
+  }
+}
+
+//Function to make images disappear at end of game
+function goodbyeCards() {
+  document.getElementById(imageTags[numberUno]).src = imageBackPath;
+  document.getElementById(imageTags[numberDos]).src = imageBackPath;
+
+  numberUno = -1;
+  numberDos = -1;
+}
+
+//Player information; add to JSON from player info page and pull info from JSON at end of game
+function thisIsPlayer() {
+
+  console.log("get player info here");
+
+  var firstName = document.getElementById("playerFirstName").value;
+  var lastName = document.getElementById("playerLastName").value;
+  var age = document.getElementById("playerAge").value;
+
+  playerInfo.firstname = playerFirstName;
+  playerInfo.lastname = playerLastName;
+  playerInfo.age = playerAge;
+
+  localStorage.setItem("readyPlayerOne", JSON.stringify(playerInfo));
+  window.location = "index.html";
+
+  window.location = "index.html";
+}
+
+function readyPlayerOne() {
+  var playerInformation = localStorage.getItem("readyPlayerOne");
+  playerInfo = JSON.parse(playerInformation);
+  var info = "Player: " + playerInfo.playerFirstName + " " + playerInfo.playerLastName + ", " + playerInfo.age + "<br>" + "Moves: " + playerInfo.move;
+  if(document.getElementById("gameFinish") != null) {
+    document.getElementById("gameFinish").innerHTML = info;
+  }
 }
