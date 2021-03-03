c416b41a8e5dd0cebe3f00168fd7579acb07704a
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-03-03 04:41:41 -0700

    work so far, am tired

---
+++ HW-8/scripts/script.js
@@ -0,0 +1,57 @@
+var mrRogersSelector = "#mrr";
+var itsHim = new Array();
+
+class RogersInfo {
+  constructor(selector, imagePath) {
+    this.selector = selector;
+    this.imagePath = imagePath;
+  }
+
+  get theSelector() {
+    return this.selector;
+  }
+
+  get theImagePath() {
+    return this.imagePath;
+  }
+
+  toString() {
+    return this.selector + ":" + this.imagePath;
+  }
+}
+
+function initializeArray() {
+  var mrr = new RogersInfo("#mrr", "./imgs/mister-rogers-01.jpg");
+  var mrr2 = new RogersInfo("#mrr", "./imgs/mister-rogers-02.jpg");
+  var mrr3 = new RogersInfo("#mrr", "./imgs/mister-rogers-03.jpg");
+
+  itsHim.push(mrr);
+  itsHim2.push(mrr2);
+  itsHim3.push(mrr3);
+  }
+
+$(document).ready(function(){
+  initializeArray();
+  console.log(itsHim.length);
+  console.log(itsHim[0].toString());
+  console.log(itsHim[0].theSelector);
+  console.log(itsHim[0].theImagePath);
+
+$(itsHim[0].theSelector).attr("src", itsHim[0].theImagePath);
+
+$("button").click(function(){
+
+  $(".words").fadeOut();
+
+  $("#special").toggle();
+    setInterval(moveObject, 1000);
+
+  $(itsHim[0].theSelector).fadeOut().fadeIn();
+
+});
+
+});
+
+function moveObject() {
+  $("#trolley").animate({left:250}).animate({top:400}).animate({left:0}).animate({top:300});
+}
