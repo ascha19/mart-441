18e60bab875795cfca733d7881b56da430dae26d
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-03-14 16:37:35 -0600

    jquery plugin = div styles

--- HW-9/scripts/script.js
+++ HW-9/scripts/script.js
@@ -1,9 +1,15 @@
 
-
+$.fn.divStyles = function(){
+  this.css("margin", "auto");
+  this.css("background-color", "rgb(187, 233, 205)");
+  this.css("border-radius", "15px");
+  this.css("width", "50%");
+};
 
 $(document).ready(function(){
     //$("body").ready(function(){
-        var imageUrl = "./imgs/animal-crossing-wp.jpg";
-        $(".background").css("background-image", "url(" + imageUrl + ")");
+        var bgUrl = "./imgs/animal-crossing-wp.jpg";
+        $(".background").css("background-image", "url(" + bgUrl + ")");
+        $("#makeDivPretty").divStyles();
     //});
 });
