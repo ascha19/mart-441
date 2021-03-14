5d753397b1f868dab9ba573807e04f6d1620d123
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-03-14 15:52:20 -0600

    FINALLY GOT BACKGROUND IMAGE TO WORK AAAAA

---
+++ HW-9/scripts/script.js
@@ -0,0 +1,9 @@
+
+
+
+$(document).ready(function(){
+    //$("body").ready(function(){
+        var imageUrl = "./imgs/animal-crossing-wp.jpg";
+        $(".background").css("background-image", "url(" + imageUrl + ")");
+    //});
+});
