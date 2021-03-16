7ed5ec529e2e76d9fa99ab2a5837b000d142d923
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-03-16 17:26:46 -0600

    initial setup

---
+++ HW-10/scripts/class.js
@@ -0,0 +1,12 @@
+class Squares {
+
+  constructor(x, y, height, width, color) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.color = color;
+  }
+
+  
+}
