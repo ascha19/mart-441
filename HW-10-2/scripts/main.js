7b5024657bb538b2329f7b3a0b0b368c94a9ba6d
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-03-19 15:24:23 -0600

    variables, gamestart button

--- HW-10-2/scripts/main.js
+++ HW-10-2/scripts/main.js
@@ -1,2 +1,19 @@
 var canvas = document.getElementById("canvasTime");
 var ctxt = canvas.getContext("2d");
+var x = 25;
+var y = 25;
+var x2 = 50;
+var y2 = 50;
+var protag;
+var goal;
+var barrier;
+var gameState = "serve";
+var attempts = 0;
+
+//Start game
+function gameStart() {
+  console.log("click");
+
+  gameState = "play";
+  document.getElementById("explain").style.display = "none";
+}
