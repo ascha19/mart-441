76fe95acac63d57cbe9f389e707552bd5a97e072
ascha19 <46611206+ascha19@users.noreply.github.com>
2021-04-23 19:50:37 -0600

    more story

--- HW-final/scripts/script.js
+++ HW-final/scripts/script.js
@@ -39,10 +39,40 @@ function getOption2() {
     document.getElementById("option3").style.display="block";
     document.getElementById("submit3").style.display="block";
 
-    myStory.innerHTML = "<p>Quickly, though, your minor inconvenience is forgotten,<br>to the point where you actually toss the cigarette butt aside.<br>What overpowers the rain now is the smell of blood<br>and the sight of the crime scene.<br>Standing off to the side are two people, one familiar and one not.<br>The familiar one is your partner in crime-solving, John Watson,<br>who waves slightly when you catch his eye;<br>the unfamiliar person next to him, a random police officer,<br>probably the one who found the body.<br><br>You can either talk to John and the officer, or examine the crime scene.<br></p>";
+    myStory.innerHTML = "<p>Quickly, though, your minor inconvenience is forgotten,<br>to the point where you actually toss the cigarette butt aside.<br>What overpowers the rain now is the smell of blood<br>and the sight of the crime scene.<br>Standing off to the side are two people, one familiar and one not.<br>The familiar one is your partner in crime-solving, John Watson,<br>who waves slightly when you catch his eye;<br>the unfamiliar person next to him, a random police officer,<br>probably the one who found the body.<br><br>You can either talk to your coworkers, or examine the crime scene.<br></p>";
   }
 
   else {
     myStory.innerHTML = "Invalid answer. Type 'Next' to continue.";
+  }
+}
+
+//Examine/Talk
+function getOption3() {
+  var myOption3 = document.getElementById("option3").value;
+  var myStory = document.getElementById("story");
+
+  if (myOption3 === "Talk") {
+    document.getElementById("option3").style.display="none";
+    document.getElementById("submit3").style.display="none";
+
+    document.getElementById("option4").style.display="block";
+    document.getElementById("submit4").style.display="block";
+
+    myStory.innerHTML = "<p>You choose to talk to your partner and the officer first.<br></p>";
+  }
+
+  else if (myOption3 === "Examine") {
+    document.getElementById("option3").style.display="none";
+    document.getElementById("submit3").style.display="none";
+
+    document.getElementById("option5").style.display="block";
+    document.getElementById("submit5").style.display="block";
+
+    myStory.innerHTML = "<p>You choose to examine the crime scene first.<br></p>";
+  }
+
+  else {
+    myStory.innerHTML = "Invalid answer. Type either 'Examine' or 'Talk' to continue.";
   }
 }
